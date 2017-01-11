f13fdeb1b08b0296ca12232d51a5afa9ef8e6968
Neekon Saadat <flygy2003@gmail.com>
2017-01-10 19:11:47 -0800

    This Is The Change Set Which Contains The Zoom Code, Optimizing The App For Smaller Barcode Readings.

--- app/src/main/java/com/example/neekondev/quickscan/MainActivity.java
+++ app/src/main/java/com/example/neekondev/quickscan/MainActivity.java
@@ -43,7 +43,24 @@ public class MainActivity
     private static final int CAMERA_PERMISSION_CAMERA = 0x000000;
 
 
+    Camera camera;
+    Camera.Parameters parameters;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        setRequestedOrientation(ActivityInfo
+                .SCREEN_ORIENTATION_PORTRAIT);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        camera = Camera.open();
+        parameters = camera.getParameters();
+        int zoom = parameters.getMaxZoom();
+        Log.d(TAG, "Zoom " + zoom);
+        int bcZoomSet = zoom / 4;
+        parameters.setZoom(bcZoomSet);
+        camera.setParameters(parameters);
 
         if (ContextCompat.checkSelfPermission(MainActivity
                 .this,
